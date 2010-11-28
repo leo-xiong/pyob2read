41a34e8e6aaa0e13a3cec548c027c46c01ad8860
ekhaliul <ekhaliul@7e6afd02-f29e-11de-945e-730681a4ffd5>
2010-11-28 00:02:11 +0000

    Many improvements to sensor management

--- trunk/pyobd2read.py
+++ trunk/pyobd2read.py
@@ -6,25 +6,30 @@ import threading
 import Queue
 import re
 from optparse import OptionParser
+from realtimeplot import RealTimeData,RealTimePlot
+import wx
 
 class OBDGui:
  def __init__(self,screen,options):
   self.screen = screen 
-  curses.halfdelay(10)
+  curses.halfdelay(1)
   self.infolines=['no port open']
   self.mode='main'
   self.options=options
+  self.currcur=-1
 
  def drawFrame(self):
   self.screen.clear()
   if self.mode=='main':self.infostr='q: quit, o: open port'
   elif self.mode=='open':self.infostr='q: quit, r: read code, c: clear code, s: read sensors'
-  elif self.mode=='sensor':self.infostr='q: quit, b: back'
+  elif self.mode=='sensor':self.infostr='q: quit, b: back, w: up,s: down, p: plot, space: toggle'
   self.screen.addstr(0,0,self.infostr)
   i=0
   for ln in self.infolines:
    self.screen.addstr(2+i,0,ln)
    i+=1
+  if self.mode=='sensor' and self.currcur>=0:
+   self.screen.addstr(2+self.currcur,0,self.infolines[self.currcur],curses.A_UNDERLINE)
   self.screen.refresh()
 
  def showSensors(self):
@@ -46,7 +51,9 @@ class OBDGui:
  def openport(self):
   try:
    self.port=obd_io.OBDPort(self.options.port)
-   self.sensorReader=obd_io.sensorReader(self.port)
+   self.port.obd_debug('Read available sensors')
+   self.sensorStr=self.port.sensor(0)[1]
+   self.sensorReader=obd_io.sensorReader(self.port,self.sensorStr)
    self.sensorReader.start()
    self.sensorReader.refresh()
    self.sensorresults={}
@@ -67,10 +74,32 @@ class OBDGui:
   self.port.clear_dtc()
   self.infolines=['DTC code erased']
 
+ def realPlot(self):
+  if self.currcur>=0:
+   sId=int(re.findall('\d+',self.infolines[self.currcur])[0])
+   aSR=obd_io.OBDsensor(self.port,sId)
+   aData=RealTimeSensorData(aSR)
+   app = wx.PySimpleApp()
+   app.frame = RealTimePlot(aData)
+   app.frame.Show()
+   app.MainLoop()
+  
+ def selectSensor(self):
+  if self.currcur>=0:
+   astr=''
+   sId=int(re.findall('\d+',self.infolines[self.currcur])[0])
+   for i in range(len(self.sensorReader.supp)):
+    if i==sId-1: 
+     astr+=str(1-int(self.sensorReader.supp[i]))
+    else: astr+=self.sensorReader.supp[i]
+   self.sensorReader.supp=astr
+   self.sensorReader.populate() 
+   
  def run(self):
+  i=0
   while True:
-   self.drawFrame()
-   c=self.screen.getch()
+   self.drawFrame() 
+   c=self.screen.getch()  
    if self.mode=='main':
     if c==ord('q'):break
     elif c==ord('o'): 
@@ -83,13 +112,36 @@ class OBDGui:
     elif c==ord('s'):
      self.infolines=['reading sensors..']
      self.mode='sensor' 
+     self.currcur=-1
      continue
     elif c==ord('q'):break
-   elif self.mode=='sensor':
+   elif self.mode=='sensor':    
+    if c==ord('q'):
+     self.sensorready=False
+     break
+    elif c==ord('b'):
+     self.mode='open'
+     self.infolines=[''] 
+    elif c==ord('w'):self.currcur=max(0,self.currcur-1)
+    elif c==ord('s'):self.currcur=min(len(self.infolines)-1,self.currcur+1)  
+    elif c==ord('p'):self.realPlot()
+    elif c==ord(' '):self.selectSensor()     
+   if self.mode=='sensor':    
     self.showSensors()
-    if c==ord('q'):break
-    elif c==ord('b'):self.mode='open'
     if self.sensorReader.queue.empty():self.sensorReader.refresh()
+
+class RealTimeSensorData(RealTimeData):
+ def __init__(self,sensor):
+  RealTimeData.__init__(self)
+  self.sensor=sensor
+ def getY(self):
+  self.sensor.read()
+  res=-1.0 #Default value  
+  try:  
+   res=float(self.sensor.value[1])
+  except:
+   return res   
+  return res
 
 def main(screen):
     a=OBDGui(screen,options)
